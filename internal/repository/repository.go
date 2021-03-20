186120fb725718605102fdeb73489e931f97c9e7
Владислав <dreamweaver3x@gmail.com>
2021-03-20 21:41:43 +0300

    added echo group and sql injection check

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"avito/internal/dateMarshaller"
 	"avito/internal/models"
+	"errors"
 	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
 )
@@ -20,12 +21,15 @@ func (s *StatsRepository) Create(stats *models.Stats) error {
 	if stats.Clicks != 0 && stats.Views != 0 {
 		stats.Cpc = stats.Cost.Div(decimal.NewFromInt(int64(stats.Clicks)))
 		stats.Cpc = stats.Cpc.Round(2)
+		stats.Cpc.StringFixedBank(2)
 	}
 
 	if stats.Views != 0 {
 		stats.Cpm = stats.Cost.Mul(decimal.NewFromInt(1000)).Div(decimal.NewFromInt(int64(stats.Views)))
 		stats.Cpm = stats.Cpm.Round(2)
+		stats.Cpm.StringFixedBank(2)
 	}
+	stats.Cost.StringFixedBank(2)
 
 	tx := s.db.Begin()
 	result := tx.Create(stats)
@@ -44,6 +48,9 @@ func (s *StatsRepository) Create(stats *models.Stats) error {
 
 func (s *StatsRepository) GetStats(from, to dateMarshaller.CustomDate, order string) ([]models.Stats, error) {
 	model := make([]models.Stats, 0)
+	if err := s.checkOrder(order); err != nil {
+		return nil, err
+	}
 	result := s.db.Order(order).Where("date BETWEEN ? and ?", from, to).Find(&model)
 	if result.Error != nil {
 		return nil, result.Error
@@ -57,4 +64,23 @@ func (s *StatsRepository) DeleteFromDB() error {
 		return result.Error
 	}
 	return nil
+}
+
+func (s *StatsRepository) checkOrder(order string) error {
+	switch order {
+	case "date":
+		return nil
+	case "clicks":
+		return nil
+	case "views":
+		return nil
+	case "cost":
+		return nil
+	case "cost_per_mille":
+		return nil
+	case "cost_per_click":
+		return nil
+	default:
+		return errors.New("invalid order")
+	}
 }
